e92ad7fa84c795bc83f0f210657c11b2bb3f8290
ShirishAryal <aryalshirish4@gmail.com>
2022-12-16 11:57:45 +0545

    theme data and router added

---
+++ lib/router/router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../ui/home_page.dart';
+
+class NewsAppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
