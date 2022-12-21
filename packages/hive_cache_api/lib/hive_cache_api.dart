9d3f4f0261ecc4eb0d05870b99e696f9eab9feed
ShirishAryal <aryal.shirish@yahoo.com>
2022-12-21 19:28:07 +0545

    Single Responsibility Principle first commit

---
+++ packages/hive_cache_api/lib/hive_cache_api.dart
@@ -0,0 +1,7 @@
+library hive_cache_api;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ packages/nivt_task_api/lib/nivt_task_api.dart
@@ -0,0 +1,4 @@
+library nivt_task_api;
+
+export './src/nivt_task_api.dart';
+export './src/models/model.dart';
